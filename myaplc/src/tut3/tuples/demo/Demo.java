98f4480423eeb52fc18e72f654b15a61eda2dd6d
kimapu <kimlee@staffemail.apu.edu.my>
2021-09-13 16:56:46 +0800

    tuple exercise

--- myaplc/src/tut3/tuples/demo/Demo.java
+++ myaplc/src/tut3/tuples/demo/Demo.java
@@ -1,10 +1,29 @@
 package tut3.tuples.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.javatuples.Pair;
+
 public class Demo {
 
+	//data-collection
+	static List<String> recordLst = Arrays.asList(
+			"Bruce,TP123456",
+			"Dory,TP987654",
+			"Gill,TP654321",
+			"Mary,TP919191"
+			);
+	
+	//TASK-1: write a function that creates a group of pair 
+		//according to the recordLst object.
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Pair<String, String> pair1 = Pair.with( "Bruce" , "TP123456" );
+		
+		
+		System.out.println( pair1 );
 	}
 
 }
