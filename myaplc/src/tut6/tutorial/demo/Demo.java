f3a95aceb0091e4b9a44cd5db0e3018b83f5ad0a
kimapu <kimlee@staffemail.apu.edu.my>
2021-10-21 18:03:26 +0800

    recursive tutorial code upload

--- myaplc/src/tut6/tutorial/demo/Demo.java
+++ myaplc/src/tut6/tutorial/demo/Demo.java
@@ -1,15 +1,43 @@
 package tut6.tutorial.demo;
 
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println( ">>> Iterative technique..." );
-		nLines( 5 );
-		
+		System.out.println(">>> Iterative technique...");
+		nLines(5);
+
 		System.out.println();
-		System.out.println( ">>> Recursive technique..." );
+		System.out.println(">>> Recursive technique...");
 		nLines2(5);
 
+	}
+
+	/**
+	 * Recursive lambda  ->
+	 * 	ternary operator 
+	 * 		? : 
+	 */
+	static final BiFunction<Integer, Integer, Long> power = 
+			(base, expo) -> expo != 0 
+				? Demo.power.apply(base, expo - 1) * base 
+				: 1;
+			
+	static final BinaryOperator<Long> power2 = 
+			(base, expo) -> expo != 0 
+				? Demo.power2.apply(base, expo - 1) * base 
+				: 1;
+	/**
+	 * Recursion function
+	 */
+	static long power(int base, int expo) {
+		if (expo != 0) {
+			return power(base, expo - 1) * base;
+		} else {
+			return 1;
+		}
 	}
 
 	/**
@@ -19,7 +47,7 @@ public class Demo {
 		if (n > 1) { // stop-case
 			nLines2(n - 1); // recursive-call
 		}
-		System.out.println(n-1);
+		System.out.println(n - 1);
 	}
 
 	/**
