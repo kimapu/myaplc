64b2ec062d60547c4cd590f58b3542ab0323105b
kimapu <kimlee@staffemail.apu.edu.my>
2021-10-25 17:19:05 +0800

    q4 and q5 answer update

--- myaplc/src/tut6/tutorial/demo/Q5.java
+++ myaplc/src/tut6/tutorial/demo/Q5.java
@@ -2,5 +2,21 @@ package tut6.tutorial.demo;
 
 public class Q5 {
 
+	/**
+	 * to find a maximum number from the given arr
+	 */
+	public static int maximum(int size, int[] arr) {
+	    if (size == 1)
+	        return arr[0];
+	    return Math.max(arr[size - 1], maximum(size - 1, arr));
+	}
+
+	public static void main(String[] args) {
+
+		int[] numArr = new int[]{2, 3, 1, 48, 6, 3, 8, 9};
+		System.out.println(maximum(numArr.length, numArr));
+		
+	}
+	
 	
 }
