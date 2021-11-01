2c699822d1e3ee54064f09e96bd7ba2a3f4e177b
kimapu <kimlee@staffemail.apu.edu.my>
2021-11-01 18:02:17 +0800

    pecs update

--- myaplc/src/tut7/tutorial/pecs/Demo.java
+++ myaplc/src/tut7/tutorial/pecs/Demo.java
@@ -5,20 +5,24 @@ import java.util.List;
 
 public class Demo {
 
+	//PECS - Producer extends, Consumer super
+	
 	//append()
-	static void append( List<? super Double> lst,  double i ) {
+	//? super Double == any unknown-type is a supertype of Double
+	static void append( List<? super Double> lst,  double i ) { //set
 		lst.add( i );
 	}
 	
 	//access()
-	static void access( List<? extends Number> lst ) {
+	//? extends Number == any unknown-type is-a-kind-of Number type
+	static void access( List<? extends Number> lst ) { //get
 		lst.forEach(System.out::println);
 	}
 	
 	public static void main(String[] args) {
 		List<Number> ints = new ArrayList<>();
 		append( ints, 100.9 );
-		ints.add(200.9);
+		ints.add(200);
 		//show
 		access(ints);
 	}
