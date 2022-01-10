7fe27115b86ca8d58a856d521b8a1d09a91f5d38
kimapu <kimlee@staffemail.apu.edu.my>
2022-01-10 11:56:49 +0800

    tut3q4 exercise

--- myaplc/src/aplc/myclass/tutorial3/Q4.java
+++ myaplc/src/aplc/myclass/tutorial3/Q4.java
@@ -1,13 +1,9 @@
 package aplc.myclass.tutorial3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import tut3.tutorial.demo.Data;
-
 public class Q4 {
 
+	/**
+	 * 4.	Sort all elements in ascending order.
+	 */
+	
 }
